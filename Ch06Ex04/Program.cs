96ea8e377d0bfec772f0c3e4758ad9d263766f2a
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-21 14:21:44 +0100

    Add project files.

---
+++ Ch06Ex04/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Ch06Ex04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{args.Length} command line arguments were specified:");
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+            Console.ReadKey();
+        }
+    }
+}
